d9c70eef5d27275447f196aa12a3ae61c7fc036d
Rasheednour <rasheednour@gmail.com>
2023-08-29 12:00:03 +0200

    Exercise 3.10: Finished

--- index.js
+++ index.js
@@ -89,6 +89,6 @@ app.post("/api/persons", (request, response) => {
   response.json(entry);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server listening on port ${PORT}`);
